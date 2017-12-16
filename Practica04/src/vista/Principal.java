dccd37eb6df61c720d87fa32259879af48ed1cc9
USER <USER@christian>
2017-12-16 09:57:27 -0500

    Creado la ventana equipo y la ventana Pais

--- Practica04/src/vista/Principal.java
+++ Practica04/src/vista/Principal.java
@@ -6,6 +6,14 @@
 package vista;
 
 import controlador.GestionDato;
+import java.util.ArrayList;
+import java.util.List;
+import modelo.Equipo;
+import modelo.Estadio;
+import modelo.Grupo;
+import modelo.Jugador;
+import modelo.Pais;
+import modelo.Torneo;
 
 /**
  *
@@ -17,11 +25,34 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        GestionDato gD = new GestionDato ();
         
-        VentanaPrincipal v = new VentanaPrincipal("Registro de Torneo",gD);
+    List<Equipo> Eq = new ArrayList<Equipo>();
+    List<Pais> Pa = new ArrayList<Pais>();
+    List<Torneo> To = new ArrayList<Torneo>();
+    List<Grupo> G = new ArrayList<Grupo>();
+    List<Estadio> E = new ArrayList<Estadio>();
+    List<Jugador> J = new ArrayList<Jugador>();
+        
+        
+        Pais p=new Pais(Long.parseLong("131"),"Ecuador","Ecuatoriana",Long.parseLong("140000"));
+        Estadio est=new Estadio(Long.parseLong("141"),"Atahualpa","Quito",10000);
+        Torneo tor=new Torneo(Long.parseLong("151"),"14/Dic/2018",est);
+        Grupo gr=new Grupo(Long.parseLong("161"),"Grupo A",4);
+        Equipo e=new Equipo(Long.parseLong("171"),25,p,tor,gr);
+        
+        Eq.add(e);
+        Pa.add(p);
+        To.add(tor);
+        E.add(est);
+        G.add(gr);
+       
+        
+        
+        GestionDato gd=new GestionDato(Eq,Pa,To,E,G,J);
+        
+        VentanaPrincipal v = new VentanaPrincipal("Archivos Binarios",gd);
         v.setVisible(true);
+        // TODO code application logic her
         
     }
     
